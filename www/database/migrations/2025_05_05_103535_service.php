b683a9772e82aced4db920949f9ddf087147605e
Timur Davidov <gemslove2@gmail.com>
2025-05-05 12:52:36 +0200

    add: добавил базовые миграции

---
+++ www/database/migrations/2025_05_05_103535_service.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('active')->default(true);
+            $table->string('title');
+            $table->text('description');
+            $table->string('filepath');
+
+            /*
+             * Пользователи
+             */
+            $table->bigInteger('userId')->unsigned()->nullable();
+            $table->foreign('userId')
+                ->references('id')
+                ->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('services');
+    }
+};
